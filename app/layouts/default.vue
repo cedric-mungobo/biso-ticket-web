239a670adcb4cbc97fcaca1b02c3a83c9835eac7
Cedric Mungobo <sedrickmungobo1@gmail.com>
2025-09-02 00:50:00 +0100

    Modification de la marge supérieure dans le layout par défaut pour une meilleure présentation
    - Réduction de la classe CSS de "pt-20" à "pt-14" pour ajuster l'espacement dans le composant de layout par défaut.
    - Amélioration de l'interface utilisateur en optimisant l'affichage des éléments.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <UiHeader/>
 
-    <div class="pt-20">
+    <div class="pt-14">
       <!-- Alerte pour les erreurs de validation API -->
       <div class="container mx-auto px-4 py-2">
         <ValidationErrorsAlert />
