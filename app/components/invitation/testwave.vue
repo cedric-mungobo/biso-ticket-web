0e1c039b5d8506c2e530e0f6ba4fc9210c665d58
Cedric Mungobo <sedrickmungobo1@gmail.com>
2025-09-15 17:11:50 +0100

    test componement

--- app/components/invitation/testwave.vue
+++ app/components/invitation/testwave.vue
@@ -2,7 +2,6 @@
     <div class="content-section">
         <div class="invitation-content">
             <h2>Invitation</h2>
-            <p>C'est avec une immense joie que nous vous annonçons notre union sacrée devant Dieu.</p>
         </div>
     </div>
 
